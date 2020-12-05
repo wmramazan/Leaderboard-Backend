16aaf871f2433499fbe5c69da368f02eece4b0f8
wmramazan <wmrmzn@gmail.com>
2020-12-05 22:16:26 +0300

    Update project for Heroku.

--- dist/user/interface/user.interface.d.ts
+++
@@ -1,7 +0,0 @@
-import { Document } from 'mongoose';
-export interface User extends Document {
-    readonly user_id: string;
-    readonly display_name: string;
-    readonly points: number;
-    readonly rank: number;
-}
